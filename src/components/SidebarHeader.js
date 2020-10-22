e80429e023b8dde21c9c70aa2d29f8c443c9cd57
Robert Petricevic <robopetricevic@gmail.com>
2020-10-22 12:54:42 +0200

    sidebar header avatar and icons

--- src/components/SidebarHeader.js
+++ src/components/SidebarHeader.js
@@ -1,9 +1,27 @@
 import React from "react";
 
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
 function SidebarHeader() {
   return (
     <div className="sidebar-header">
-      <div className="sidebar-header-top">Top</div>
+      <div className="sidebar-header-top">
+        <Avatar />
+        <div className="icons-box">
+          <IconButton size="small" className={"MyCustomButton"}>
+            <DonutLargeIcon fontSize="small" />
+          </IconButton>
+          <IconButton size="small" className={"MyCustomButton"}>
+            <ChatIcon fontSize="small" />
+          </IconButton>
+          <IconButton size="small" className={"MyCustomButton"}>
+            <MoreVertIcon fontSize="small" />
+          </IconButton>
+        </div>
+      </div>
       <div className="sidebar-header-input">Input</div>
     </div>
   );

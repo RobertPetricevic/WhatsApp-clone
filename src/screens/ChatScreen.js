118d2647599b70af0b0a79f1893d21494c208126
Robert Petricevic <robopetricevic@gmail.com>
2020-10-22 15:57:18 +0200

    chat footer

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -4,13 +4,16 @@ import { Avatar, IconButton } from "@material-ui/core";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchIcon from "@material-ui/icons/Search";
 import MessageBox from "../components/MessageBox";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import MicIcon from "@material-ui/icons/Mic";
 
 function ChatScreen() {
   if (false) {
     return <div className="no-chat">Click on chat in sidebar</div>;
   }
   return (
-    <div className="chat">
+    <div className="chat-screen">
       <div className="chat-header">
         <div className="room-info">
           <Avatar />
@@ -33,7 +36,27 @@ function ChatScreen() {
         <MessageBox message="Blbla bla" />
         <MessageBox message="Bla a bla bla bla bla bla" receiver />
       </div>
-      <div className="chat-footer">footer</div>
+      <div className="chat-footer">
+        <IconButton size="small" className={"MyCustomButton"}>
+          <InsertEmoticonIcon fontSize="medium" />
+        </IconButton>
+        <IconButton size="small" className={"MyCustomButton"}>
+          <AttachFileIcon fontSize="medium" />
+        </IconButton>
+        <form className="footer-form">
+          <textarea
+            className="footer-input"
+            type="text"
+            placeholder="Type a message"
+          />
+          <button className="footer-btn" type="submit">
+            Submit
+          </button>
+        </form>
+        <IconButton size="small" className={"MyCustomButton"}>
+          <MicIcon fontSize="medium" />
+        </IconButton>
+      </div>
     </div>
   );
 }
